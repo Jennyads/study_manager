52195b205e4bf57b65e5db43b605d46ead5dc5bd
Jennyads <jennycristina.1998@gmail.com>
2023-07-03 11:36:45 -0300

    refactor code to have only function

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,17 +1,49 @@
-import React from 'react';
-import Form from '../components/Form'
-import List from '../components/List';
-import style from './App.module.scss';
+import React, { useState } from 'react';
 import Cronometro from '../components/Cronometro';
-
+import Formulario from '../components/Formulario';
+import Lista from '../components/Lista';
+import { ITarefa } from '../types/tarefa';
+import style from './App.module.scss';
 
 function App() {
+  const [tarefas, setTarefas] = useState<ITarefa[]>([]);
+  const [selecionado, setSelecionado] = useState<ITarefa>();
+
+  function selecionaTarefa(tarefaSelecionada: ITarefa) {
+    setSelecionado(tarefaSelecionada);
+    setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa => ({
+      ...tarefa,
+      selecionado: tarefa.id === tarefaSelecionada.id ? true : false
+    })))
+  }
+
+  function finalizarTarefa() {
+    if(selecionado) {
+      setSelecionado(undefined);
+      setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa => {
+        if(tarefa.id === selecionado.id) {
+          return {
+            ...tarefa,
+            selecionado: false,
+            completado: true
+          }
+        }
+        return tarefa;
+      }))
+    }
+  }
+
   return (
     <div className={style.AppStyle}>
-      <Form />
-      <List />
-      <Cronometro />
-     
+      <Formulario setTarefas={setTarefas} />
+      <Lista
+        tarefas={tarefas}
+        selecionaTarefa={selecionaTarefa}
+      />
+      <Cronometro
+        selecionado={selecionado}
+        finalizarTarefa={finalizarTarefa}
+      />
     </div>
   );
 }
